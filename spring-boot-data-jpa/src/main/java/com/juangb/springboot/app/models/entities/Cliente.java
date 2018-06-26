3171db357888adf973ac4ea596b05cbb25ca4a1d
Gabo32 <j.gabriel.b@live.com.mx>
2018-06-25 23:38:48 -0500

    Proyecto JPA
    Creacion de proyecto jpa

---
+++ spring-boot-data-jpa/src/main/java/com/juangb/springboot/app/models/entities/Cliente.java
@@ -0,0 +1,58 @@
+package com.juangb.springboot.app.models.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Cliente implements Serializable{
+	private Long id;
+	
+	private String name;
+	
+	private String apellido;
+	
+	private String email;
+	
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	
+}
