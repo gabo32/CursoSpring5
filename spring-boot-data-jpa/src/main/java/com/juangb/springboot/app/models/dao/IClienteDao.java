bd3a498fabd44b54d240dc1e63e8b89d94a5af08
Gabo32 <j.gabriel.b@live.com.mx>
2018-06-28 23:09:04 -0500

    Camiob dao por crudrepository
    se elimina la clase datoimpl y se utiliza la interfaz crud repository

--- spring-boot-data-jpa/src/main/java/com/juangb/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/juangb/springboot/app/models/dao/IClienteDao.java
@@ -2,14 +2,21 @@ package com.juangb.springboot.app.models.dao;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
 import com.juangb.springboot.app.models.entities.Cliente;
 
-public interface IClienteDao {
-	public List<Cliente> findAll();
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
 	
-	public void save(Cliente cliente);
 	
-	public Cliente findOne(Long id);
 	
-	public void delete(Long id);
+	
+	//estos metodos ya no se usan
+//	public List<Cliente> findAll();
+//	
+//	public void save(Cliente cliente);
+//	
+//	public Cliente findOne(Long id);
+//	
+//	public void delete(Long id);
 }
